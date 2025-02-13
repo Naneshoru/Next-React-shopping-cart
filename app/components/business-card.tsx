4746a3f18d0666a08f342625831b182d41a876a3
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-02-12 21:38:13 -0300

    feat: Loading local usando suspense

--- app/components/business-card.tsx
+++ app/components/business-card.tsx
@@ -1,21 +1,11 @@
 'use client'
 
 import { useTheme } from 'next-themes';
-import React, { useEffect, useState } from 'react';
-import Loading from '@app/loading';
+import React from 'react';
+// import Loading from '@app/loading';
 
 export default function BusinessCard ({ children }: { children: React.ReactNode }) {
-  const { resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  console.log(resolvedTheme)
-  // Ensure the component only renders after the theme has been resolved
-  if (!mounted) {
-    return <Loading />; // or a loading spinner
-  }
+  const { resolvedTheme = 'dark' } = useTheme();
 
   return (
     <div className={`flex flex-col mx-auto gap-6 p-12 rounded-xl sm:max-w-96
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
 import BusinessCard from "./components/business-card";
 import Link from "next/link";
 import Header from "@app/components/header";
-import { use } from "react";
+import PokeData from "./pokedata";
+import { Suspense } from "react";
 
 export default function Home() {
-  const fetchWeather = async () => {
-
-    // const API_KEY = '0d4f9ba8eadb4d26a59135439251601'
-    // const BASE_URL = 'http://api.weatherapi.com/v1/current.json'
-    
-  
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-      if (!response.ok) {
-        throw new Error('Failed to fetch weather');
-      }
-      return await response.json();
-  }
-
-  const weather = use(fetchWeather());
 
   return (
     <div className="bg-black bg-home-image bg-cover bg-center flex flex-col h-dvh">
@@ -25,7 +12,9 @@ export default function Home() {
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-full">
         <BusinessCard >
           <h1 className="text-4xl font-bold">Ricardo&apos;s Shop</h1>
-    {JSON.stringify(weather.name)}
+          <Suspense fallback={<p>Loading local...</p>}>
+            <PokeData />
+          </Suspense>
           <address>
             53 S McDonnell Rd <br/>
             San Francisco, CA 94128, United States
